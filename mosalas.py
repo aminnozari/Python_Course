b8e4f702a37d9630b898dbcc4ac820ced0abc535
Amin Nozari <41434431+aminnozari@users.noreply.github.com>
2022-07-13 01:37:35 +0430

    Add files via upload

---
+++ mosalas.py
@@ -0,0 +1,8 @@
+print("andaze zel haye mosalas ra vared konid")
+a=int(input())
+b=int(input())
+c=int(input())
+if a<(b+c) and b<(a+c) and c<(a+b):
+  print("mosalas ghabele rasm ast")
+else:
+    print("mosalas ghabele rasm nist")
